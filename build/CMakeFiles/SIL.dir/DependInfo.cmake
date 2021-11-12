4a5f2f8007be47f5a03751118c6f7115cf65de8b
zhixiang.qian <zhixiang.qian@imotion.ai>
2021-11-12 19:58:22 +0800

    add

---
+++ build/CMakeFiles/SIL.dir/DependInfo.cmake
@@ -0,0 +1,24 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/home/qzx/code/SIL/src/detect/detector.cpp" "CMakeFiles/SIL.dir/src/detect/detector.cpp.o" "gcc" "CMakeFiles/SIL.dir/src/detect/detector.cpp.o.d"
+  "/home/qzx/code/SIL/src/postprocess/postprocessor.cpp" "CMakeFiles/SIL.dir/src/postprocess/postprocessor.cpp.o" "gcc" "CMakeFiles/SIL.dir/src/postprocess/postprocessor.cpp.o.d"
+  "/home/qzx/code/SIL/src/preprocess/preprocessor.cpp" "CMakeFiles/SIL.dir/src/preprocess/preprocessor.cpp.o" "gcc" "CMakeFiles/SIL.dir/src/preprocess/preprocessor.cpp.o.d"
+  "/home/qzx/code/SIL/src/recognise/recogniser.cpp" "CMakeFiles/SIL.dir/src/recognise/recogniser.cpp.o" "gcc" "CMakeFiles/SIL.dir/src/recognise/recogniser.cpp.o.d"
+  "/home/qzx/code/SIL/src/runable.cpp" "CMakeFiles/SIL.dir/src/runable.cpp.o" "gcc" "CMakeFiles/SIL.dir/src/runable.cpp.o.d"
+  "/home/qzx/code/SIL/src/visualize/visualization.cpp" "CMakeFiles/SIL.dir/src/visualize/visualization.cpp.o" "gcc" "CMakeFiles/SIL.dir/src/visualize/visualization.cpp.o.d"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ build/CMakeFiles/SIL.dir/cmake_clean.cmake
@@ -0,0 +1,21 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/SIL.dir/src/detect/detector.cpp.o"
+  "CMakeFiles/SIL.dir/src/detect/detector.cpp.o.d"
+  "CMakeFiles/SIL.dir/src/postprocess/postprocessor.cpp.o"
+  "CMakeFiles/SIL.dir/src/postprocess/postprocessor.cpp.o.d"
+  "CMakeFiles/SIL.dir/src/preprocess/preprocessor.cpp.o"
+  "CMakeFiles/SIL.dir/src/preprocess/preprocessor.cpp.o.d"
+  "CMakeFiles/SIL.dir/src/recognise/recogniser.cpp.o"
+  "CMakeFiles/SIL.dir/src/recognise/recogniser.cpp.o.d"
+  "CMakeFiles/SIL.dir/src/runable.cpp.o"
+  "CMakeFiles/SIL.dir/src/runable.cpp.o.d"
+  "CMakeFiles/SIL.dir/src/visualize/visualization.cpp.o"
+  "CMakeFiles/SIL.dir/src/visualize/visualization.cpp.o.d"
+  "SIL"
+  "SIL.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/SIL.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
