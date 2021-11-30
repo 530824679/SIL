a0fd9466d5edd19cd65e916fd01366ce23796484
chenwei <wei.chen@imotion.ai>
2021-11-30 23:03:24 +0800

    merge local code into remote

---
+++ build/CMakeFiles/SIL.dir/DependInfo.cmake
@@ -0,0 +1,34 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/home/chenwei/Project/SIL/src/detect/detector.cpp" "/home/chenwei/Project/SIL/build/CMakeFiles/SIL.dir/src/detect/detector.cpp.o"
+  "/home/chenwei/Project/SIL/src/postprocess/postprocessor.cpp" "/home/chenwei/Project/SIL/build/CMakeFiles/SIL.dir/src/postprocess/postprocessor.cpp.o"
+  "/home/chenwei/Project/SIL/src/preprocess/preprocessor.cpp" "/home/chenwei/Project/SIL/build/CMakeFiles/SIL.dir/src/preprocess/preprocessor.cpp.o"
+  "/home/chenwei/Project/SIL/src/recognise/recogniser.cpp" "/home/chenwei/Project/SIL/build/CMakeFiles/SIL.dir/src/recognise/recogniser.cpp.o"
+  "/home/chenwei/Project/SIL/src/runable.cpp" "/home/chenwei/Project/SIL/build/CMakeFiles/SIL.dir/src/runable.cpp.o"
+  "/home/chenwei/Project/SIL/src/visualize/visualization.cpp" "/home/chenwei/Project/SIL/build/CMakeFiles/SIL.dir/src/visualize/visualization.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  ".././include/detect"
+  ".././include/recognise"
+  ".././include/track"
+  ".././include/common"
+  ".././include/preprocess"
+  ".././include/postprocess"
+  ".././include/visualize"
+  ".././include/onnxruntime/cpu"
+  "/usr/include/opencv"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
